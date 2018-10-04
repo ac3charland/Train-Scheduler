67749f34256d1c8984bd55e0823491afa499984a
Alex Charland <ac3charland@gmail.com>
2018-10-04 18:21:18 -0500

    Set up Firebase as well.

---
+++ assets/javascript/logic.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyCckO5Pnbd05cxxCJhrWeQCr0yBjhPYAzc",
+    authDomain: "train-scheduler-a48f3.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a48f3.firebaseio.com",
+    projectId: "train-scheduler-a48f3",
+    storageBucket: "train-scheduler-a48f3.appspot.com",
+    messagingSenderId: "293730820034"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
